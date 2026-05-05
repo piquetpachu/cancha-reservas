517205b8c4f278662680fc534941614601884b4b
Caroo_G <caroobg15@gmail.com>
2026-05-05 20:26:17 -0300

    ocultar botones para clientes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,38 @@
 import { useNavigate } from 'react-router-dom'
 import { logout } from '../services/authService'
+import { useEffect, useState } from 'react'
+import { supabase } from '../supabaseClient'
 
 export default function Navbar() {
   const navigate = useNavigate()
+  const [rol, setRol] = useState(null)
+
+  useEffect(() => {
+    
+    async function getRol() {
+      const { data: userData } = await supabase.auth.getUser()
+      const user = userData.user
+
+      if (!user) return
+
+      const { data, error } = await supabase
+        .from("profiles")
+        .select("rol")
+        .eq("id", user.id)
+
+      console.log("DATA:", data)
+      console.log("ERROR:", error)
+
+      if (data && data.length > 0) {
+        const rolDB = data[0].rol?.toLowerCase()
+        console.log("ROL FINAL:", rolDB)
+
+        setRol(rolDB)
+      }
+    }
+
+    getRol()
+  }, [])
 
   async function handleLogout() {
     await logout()
@@ -27,13 +57,18 @@ export default function Navbar() {
         </button>
       </div>
 
-      <button onClick={() => navigate('/crear-club')}>
-        Crear Club
-      </button>
+      {/* SOLO DUEÑO O ADMIN */}
+      {(rol === "dueno" || rol === "dueño" || rol === "admin") && (
+        <>
+          <button onClick={() => navigate('/crear-club')}>
+            Crear Club
+          </button>
 
-      <button onClick={() => navigate('/crear-cancha')}>
-        Crear Cancha
-      </button>
+          <button onClick={() => navigate('/crear-cancha')}>
+            Crear Cancha
+          </button>
+        </>
+      )}
 
       <button onClick={handleLogout}>
         Cerrar sesión
